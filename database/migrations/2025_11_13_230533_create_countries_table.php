d162c894633ae0f6c00eafd4c25689b68c12b99d
Secret198 <96205404+Secret198@users.noreply.github.com>
2025-11-14 01:58:22 +0200

    database migrations, and seeders setup

---
+++ database/migrations/2025_11_13_230533_create_countries_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string("code");
+            $table->string("name");
+            $table->string("capital");
+            $table->string("passport_validity");
+            $table->string("phone_code");
+            $table->string("timezone");
+            $table->unsignedBigInteger("population");
+            $table->unsignedBigInteger("area_km2");
+            $table->string("embassy_url");
+            $table->string("registration")->nullable();
+            $table->string("registration_url")->nullable();
+            $table->string("primary_rule")->nullable();
+            $table->string("primary_rule_duration")->nullable();
+            $table->string("secondary_rule")->nullable();
+            $table->string("secondary_rule_duration")->nullable();
+            $table->unsignedBigInteger("continent_id");
+            $table->unsignedBigInteger("currency_id");
+
+            $table->foreign("currency_id")->references("id")->on("currencies")->onDelete("cascade");
+            $table->foreign("continent_id")->references("id")->on("continents")->onDelete("cascade");;
+                        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Helper\ProgressBar;
+use function Laravel\Prompts\table;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +20,63 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $filePath = storage_path('../countries.csv');
+
+        $continents = array();
+        $currencies = array();
+        if (($handle = fopen($filePath, 'r')) !== false) {
+           
+            while (($data = fgetcsv($handle, separator:";")) !== false) {
+                if(!array_key_exists($data[4], $currencies)){
+                    array_push($currencies, $data[4]);
+                    array_push($currencies, $data[5]);
+                }
+
+                if(!in_array($data[2], $continents)){
+                    array_push($continents, $data[2]);
+                }
+
+            }
+            fclose($handle);
+        }
+        for($i = 0;$i<count($currencies);$i+=2){
+            DB::table('currencies')->insert([
+                "code" => $currencies[$i],
+                "name" => $currencies[$i+1]
+            ]);
+        }
+        for($i = 0;$i<count($continents);$i++){
+            DB::table('continents')->insert([
+                "name" => $continents[$i]
+            ]);
+        }
+
+        if (($handle = fopen($filePath, 'r')) !== false) {
+           
+            while (($data = fgetcsv($handle, separator:";")) !== false) {
+
+                DB::table("countries")->insert([
+                    "code" => $data[0],
+                    "name" => $data[1],
+                    "capital" => $data[3],
+                    "passport_validity" => $data[6],
+                    "phone_code" => $data[7],
+                    "timezone" => $data[8],
+                    "population" => $data[9],
+                    "area_km2" => $data[10],
+                    "embassy_url" => $data[11],
+                    "registration" => $data[12],
+                    "registration_url" => $data[13],
+                    "primary_rule" => $data[14],
+                    "primary_rule_duration" => $data[15],
+                    "secondary_rule" => $data[16],
+                    "secondary_rule_duration" => $data[17],
+                    "continent_id" => array_search($data[2], $continents) + 1,
+                    "currency_id" => array_search($data[4], $currencies) / 2 + 1,
+                ]);
+
+            }
+            fclose($handle);
+        }
     }
 }
