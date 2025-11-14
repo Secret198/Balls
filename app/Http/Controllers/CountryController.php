dbd4157e1e1ddb5e9a2e45a7e3837093767a8f6a
Secret198 <96205404+Secret198@users.noreply.github.com>
2025-11-14 14:09:40 +0200

    error handling

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -18,6 +18,7 @@ class CountryController extends Controller
     public function search(string $countryId): View{
         $country = Country::select("*")->where("countries.id", $countryId)->join('continents', 'countries.continent_id', '=', 'continents.id')
         ->join('currencies', 'countries.currency_id', '=', 'currencies.id')->get();
+
         return view('country_view', compact("country"));
     }
 }
